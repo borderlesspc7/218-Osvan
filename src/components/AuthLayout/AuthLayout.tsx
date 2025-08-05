d026157f456fccb88a9d4ea3229b60bad6989a90
Matheus Corbellini <matheussteindorff@gmail.com>
2025-08-05 18:29:12 -0300

    footer da borderless

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,15 +1,20 @@
 import "./AuthLayout.css";
 import React from "react";
-
+import { Footer } from "borderless";
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   return (
-    <div className="auth-page">
-      <div className="auth-container">{children}</div>
-    </div>
+    <>
+      <div className="auth-page">
+        <div className="auth-container">{children}</div>
+      </div>
+      <div className="footer">
+        <Footer backgroundColor="transparent" />
+      </div>
+    </>
   );
 };
 
